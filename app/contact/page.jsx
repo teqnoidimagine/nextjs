6c0528d2de725190bd0149fc5b0f765c49371147
teqnoidimagine <dheeraj@teqnoid.com>
2024-10-22 17:27:30 +0530

    sasq2

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -12,7 +12,7 @@ export default function Contact() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Curve backgroundColor="#6E97D0">
-        <h1>Contact</h1>
+        <h1>Contact1</h1>
         <div className='body'>
             <p>Fusce tristique nisi eu sodales suscipit. Praesent ante ipsum, suscipit eu dapibus in, tincidunt et massa. Sed pulvinar consequat elit at placerat. Maecenas bibendum in purus sed hendrerit. Integer efficitur mattis consectetur. Sed sagittis tortor non mauris laoreet ultrices. Nunc sit amet sem suscipit, accumsan justo sed, laoreet odio. Phasellus pharetra arcu nibh, non scelerisque ipsum rutrum quis. Fusce quis commodo leo. Mauris ut congue mauris.</p>
             <p>Duis tempus risus ut est malesuada, sit amet auctor tortor tempus. Sed luctus, odio a vulputate elementum, nulla enim bibendum ipsum, in ornare elit nisi quis tortor. Nullam quis condimentum tellus. Nullam non eros sem. Cras molestie convallis dolor, quis egestas turpis congue in. Maecenas ut tellus at nunc consectetur facilisis. Vivamus ornare ex a nibh egestas, id euismod mi euismod.</p>
